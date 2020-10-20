76838405ceda829dc91263e22234159a8641961b
BerTepesh <Ber.Tepesh@gmail.com>
2020-10-20 22:46:10 +0300

    validation

--- src/js/main.js
+++ src/js/main.js
@@ -18,11 +18,45 @@ $('.confirm__nums input').bind("change keyup input click", function(e) {
   }
 });
 
+var phone = $('#phone');
+var password_reg = $('#password');
+$('#registration input').blur(function(){
+  var nums_confirm = true;
+  $('#confirm_num li').each(function(){
+    if($(this).find('input').val() == '') {
+      nums_confirm = false;
+    }
+  });
+  console.log(phone.val());
+  if(!nums_confirm) {
+    $('.confirm__nums').removeClass('valid').addClass('invalid');
+  } else {
+    $('.confirm__nums').removeClass('invalid').addClass('valid');
+  }
+  if(phone.val() != '' && password_reg.val() != '' && nums_confirm){
+    $('#submit-reg').attr('disabled', false);
+  } else {
+    $('#submit-reg').attr('disabled', true);
+  }
+});
+
+
+var phone2 = $('#phone2');
+var password2 = $('#password2');
+password2.blur(function(){
+  if(phone2.val() != '' && password2.val() != ''){
+    $('#submit1').attr('disabled', false);
+  } else {
+    $('#submit1').attr('disabled', true);
+  }
+});
+
 var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
 var mail = $('#mail');
+var password3 = $('#password3');
 
-mail.blur(function(){
-  if(mail.val() != ''){
+password3.blur(function(){
+  if(mail.val() != '' && password3.val() != ''){
     if(mail.val().search(pattern) == 0) {
       $('#valid').text('');
       $('#submit').attr('disabled', false);
@@ -39,8 +73,8 @@ mail.blur(function(){
   }
 });
 
-var phone = $('#phone4');
-phone.blur(function(){
+var phone4 = $('#phone4');
+phone4.blur(function(){
   if(phone.val() != ''){
     $('#submit2').attr('disabled', false);
   } else {
@@ -75,4 +109,16 @@ mail2.blur(function(){
 $('#recover-mail-done').on('show.bs.modal', function (event) {
   var mail = $('#mail2');
   $('#recover-mail-text').text(mail.val());
+});
+
+var pass4 = $('#password4');
+var pass5 = $('#password5');
+$('#new-pass input').blur(function(){
+  if(pass4.val() != '' && pass5.val() != '' && pass4.val() == pass5.val()){
+    $('#save_pass').attr('disabled', false);
+    $('#valid-pass').text('');  
+  } else {
+    $('#save_pass').attr('disabled', true);
+    $('#valid-pass').text('Пароли должны совпадать');  
+  }
 });
