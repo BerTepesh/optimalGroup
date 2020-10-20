92e61c9c4b7cf400de33b149734c8ecd1fc0ffa4
BerTepesh <Ber.Tepesh@gmail.com>
2020-10-20 20:28:11 +0300

    authorisation

--- optimalGroup/js/main.js
+++ optimalGroup/js/main.js
@@ -12,9 +12,31 @@ $('.confirm__nums input').keypress(function(){
     return false;
   }
 })
-
 $('.confirm__nums input').bind("change keyup input click", function(e) {
   if (this.value.match(/[^0-9]/g)) {
    this.value = this.value.replace(/[^0-9]/g, '');
   }
 });
+
+ $(document).ready(function(){
+  var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+  var mail = $('#mail');
+  
+  mail.blur(function(){
+    if(mail.val() != ''){
+      if(mail.val().search(pattern) == 0) {
+        $('#valid').text('');
+        $('#submit').attr('disabled', false);
+        mail.removeClass('error').addClass('ok');
+      } else {
+        $('#valid').text('Неверные данные. Проверьте, нет ли опечаток');
+        $('#submit').attr('disabled', true);
+        mail.addClass('error');
+      }
+    } else {
+      $('#valid').text('Поле e-mail не должно быть пустым!');
+      mail.addClass('error');
+      $('#submit').attr('disabled', true);
+    }
+  });
+ });
